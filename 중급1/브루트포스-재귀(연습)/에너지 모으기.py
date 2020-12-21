2af5bc03712aefcb74fab26efabe5fa18c449717
VIXXPARK <vixx170627@gmail.com>
2020-12-21 09:40:11 +0900

    재귀 2문제 저녁에 다시 풀기

--- 중급1/브루트포스-재귀(연습)/에너지 모으기.py
+++ 중급1/브루트포스-재귀(연습)/에너지 모으기.py
@@ -38,3 +38,13 @@ func(0,ans)
 print(ret)
 
 ## 좀 느리므로 다른 코드 참조
+n=int(input())
+L=list(map(int,input().split()))
+def f(L):
+    if len(L)==3: return L[0]*L[2]
+    ret=0
+    for i in range(1,len(L)-1):
+        r=L[i-1]*L[i+1]+f(L[:i]+L[i+1:])
+        ret=max(ret,r)
+    return ret
+print(f(L))
