2cfdded3c1c57edc02a9d09b4c2fbc606ac8bfad
VIXXPARK <vixx170627@gmail.com>
2020-12-27 09:16:00 +0900

    복습 완료

--- 중급1/브루트 포스-비트마스크(연습)/2048(easy).py
+++ 중급1/브루트 포스-비트마스크(연습)/2048(easy).py
@@ -107,4 +107,62 @@ def dfs(ceil,board,travel):
     
 dfs(0,graph,[])
 print(ans)
+
 ## 빠른 코드 참조
+
+from itertools import chain
+import sys
+read = sys.stdin.readline
+
+n = int(read().strip())
+blocks = [list(map(int, read().strip().split())) for _ in range(n)]
+
+
+def move_blocks(now_blocks, depth, direction):
+    # 위 아래 경우에는 transpose
+    if direction == 0 or direction == 1:
+        now_blocks = list(map(list, zip(*now_blocks)))
+
+    nxt_blocks = []
+    for line in now_blocks:
+        non_zero = [i for i in line if i] ##0이 아닌 값들만 추출
+
+        # 왼쪽으로 move
+        if direction == 0 or direction == 3:
+            for i in range(1, len(non_zero)):
+                if non_zero[i-1] == non_zero[i]:
+                    non_zero[i-1] += non_zero[i]
+                    non_zero[i] = 0
+
+            non_zero = [i for i in non_zero if i]
+            non_zero = non_zero + [0]*(n-len(non_zero))
+
+        else:
+            for i in range(len(non_zero)-1, 0, -1):
+                if non_zero[i-1] == non_zero[i]:
+                    non_zero[i] += non_zero[i-1]
+                    non_zero[i-1] = 0
+
+            non_zero = [i for i in non_zero if i]
+            non_zero = [0] * (n - len(non_zero)) + non_zero
+
+        nxt_blocks.append(non_zero)
+
+    if direction == 0 or direction == 1:
+        nxt_blocks = list(map(list, zip(*nxt_blocks)))
+
+    if depth == 5:
+        global max_value
+        max_value = max(max_value, max(list(chain(*nxt_blocks))))
+        return
+
+    move_blocks(nxt_blocks, depth+1, 0)
+    move_blocks(nxt_blocks, depth+1, 1)
+    move_blocks(nxt_blocks, depth+1, 2)
+    move_blocks(nxt_blocks, depth+1, 3)
+
+
+max_value = 0
+for i in range(4):
+    move_blocks(blocks, 1, i)
+print(max_value)
