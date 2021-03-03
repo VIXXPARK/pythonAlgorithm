12540bb5a66b6e70f1e8878230102f6d27a2e0c2
Suhan Park <vixx170627@gmail.com>
2021-03-03 10:29:25 +0900

    코드가 괜히 길어질 때 틀렸다는 것을 알아야한다.

--- pad.py
+++ pad.py
@@ -1,3 +1,49 @@
-lst=[[6,1-1],[5,1-1],[4,1-1],[3,3-1],[2,3-1],[1,5-1]]
-lst=sorted(lst,key=lambda x : (1/(x[1]+1),x[0]),reverse=True)
-print(lst)
+# def solution(prices):
+#     answer = []
+#     prices=prices[::-1]
+#     val=[-1 for _ in range(max(prices)+1)]
+#     for idx,x in enumerate(prices):
+#         flag=True
+#         minval=0
+#         for i in range(x-1,-1,-1):
+#             if val[i]!=-1:
+#                 flag=False
+#                 minval=max(minval,val[i])
+#         if flag:
+#             answer.insert(0,idx)
+#         else:
+#             answer.insert(0,idx-minval)
+#         val[x]=idx
+#     return answer
+
+
+
+def solution(prices):
+    answer = []
+    count=0
+    val=100000
+    for i in range(len(prices)-1,-1,-1):
+        if count==0:
+            answer.insert(0,0)
+            val=prices[i]
+            count+=1
+        else:
+            if val<prices[i]:
+                x=-1
+                for idx in range(1,count+1):
+                    if prices[i]>prices[i+idx]:
+                        x=idx
+                        break
+                count+=1
+                if x==-1:
+                    answer.insert(0,count-1)
+                else:
+                    answer.insert(0,x)
+            else:
+                count+=1
+                answer.insert(0,count-1)
+                val=prices[i]
+
+    return answer
+
+print(solution([1, 2, 3, 2, 3]))
---
+++ 프로그래머스/레벨2/주식가격.py
@@ -0,0 +1,29 @@
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
+############################################
+# 간단하게 생각하는 연습이 필요한 것 같다. 숫자 범위에 고려해서 처음에
+# 생각한 방법을 제외하고 했는데 알고보니 처음 생각한 방법이 통과되는 코드일 줄은 ....
+from collections import deque
+def solution2(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
