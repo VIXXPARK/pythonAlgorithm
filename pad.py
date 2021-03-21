ad9570ca3707037905d1258651d4e53f33b44fc3
Suhan Park <vixx170627@gmail.com>
2021-03-22 07:19:59 +0900

    단순한 정렬문제

---
+++ pad.py
@@ -0,0 +1,12 @@
+import collections
+nums = [3,2,3,1,2,4,5,5,6]
+# length=len(nums)
+# nums=collections.Counter(nums).most_common()
+# nums.sort(key=lambda x: x[0],reverse=True)
+# count=0
+# store=0
+# while count<4:
+# 	store+=nums[count][1]
+# 	count+=1
+# print(length-store+1)
+print(sorted(nums))
