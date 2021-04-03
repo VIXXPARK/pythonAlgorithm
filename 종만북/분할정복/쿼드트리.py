2e6b5157159bed44b13d1881e6a56bce554d35b5
Suhan Park <vixx170627@gmail.com>
2021-04-03 21:50:24 +0900

    iter라는 키워드 활용 c++에서 흔히 봤던 그 iterator와 유사

--- 종만북/분할정복/쿼드트리.py
+++ 종만북/분할정복/쿼드트리.py
@@ -1,21 +1,15 @@
-#quadtree
-def quad(loctree, ind):
-    let = loctree[ind]  # 인덱스 번째의 글자
-    if let == 'w' or let == 'b':  # 상하 바꿔도 변함이 없음
-        return let
-    # let이 w나 b가 아닌 경우=let이 x일때
-    ind += 1  # x가 나온 위치로부터 1칸 뒤
-    a=quad(tree,ind)
-    ind += len(a)  # 왼쪽 위가 차지하는 칸수 만큼 뒤
-    b=quad(tree,ind)
-    ind += len(b)  # 오른 위가 차지하는 칸수 만큼 뒤
-    c=quad(tree,ind)
-    ind += len(c)  # 왼쪽 아래가 차지하는 칸수 만큼 뒤
-    d=quad(tree,ind)
-    return 'x'+c+d+a+b
- 
- 
-case = int(input())
-for i in range(case):
-    tree = input()
-    print(quad(tree, 0))  # 인덱스=0
+board=["x","b","w","x","w","b","b","w","b"]
+y=iter(board)
+def func(it:iter):
+    try:
+        head=next(it)
+        if head=='b' or head == 'w':
+            return head
+        upperLeft=func(it)
+        upperRight=func(it)
+        lowerLeft=func(it)
+        lowerRight=func(it)
+        return "x"+lowerLeft+lowerRight+upperLeft+upperRight
+    except:
+        pass    
+print(func(y))
