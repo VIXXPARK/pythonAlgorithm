e9b52d28eb80dcbaee9014e2fc6ad4333f1ba50e
Suhan Park <vixx170627@gmail.com>
2021-06-04 11:11:38 +0900

    deque의 활용 sliding window

--- leetcode/85_edit_distance.py
+++ leetcode/85_edit_distance.py
@@ -53,7 +53,7 @@ def minDistance(self, word1, word2):
     pre = [i for i in range(w)]
     for i in range(1, h):
         cur = [i for _ in range(w)]
-        for j in range(1, w):
+        for j in range(1, w):  # replace or sync / delete / insert
             cur[j] = min(pre[j-1]+(word1[i-1] != word2[j-1]),
                          pre[j]+1, cur[j-1]+1)
         pre = cur
